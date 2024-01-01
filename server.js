ff535a9eb3b0e6c56c19c8936d08e14220cdec81
hieuNV0871 <hieunv0871@gmail.com>
2024-01-01 19:00:29 +0700

    ok

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ db.connect();
 //   },
 // };
 app.use(cors({
-  origin: ["https://nvhshop.onrender.com","https://nvhshop-adpanel.onrender.com"],
+  origin: ["https://nvhshop.onrender.com","https://nvhshop-adpanel.onrender.com", "http://localhost:3001"],
   // headers: ["Content-Type"],
   credentials: true,
 }));
@@ -36,7 +36,7 @@ route(app)
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["https://nvhshop.onrender.com","https://nvhshop-adpanel.onrender.com"]
+    origin: ["https://nvhshop.onrender.com","https://nvhshop-adpanel.onrender.com" ,"http://localhost:3001"]
   }
 }); 
 
